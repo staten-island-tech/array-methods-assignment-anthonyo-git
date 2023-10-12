c353ecf972c877c54e69330065ac067954a9dd6b
anthonyo-git <124058513+anthonyo-git@users.noreply.github.com>
2023-10-12 09:19:09 -0400

    80%

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const president = [{name: 'Washington', yeas:[1789,1797]},]
+const president = [{name: 'Washington', years:[1789,1790,1791,1792,1793,1794,1795,1796,1797]}, 
+{name: 'Abraham Lincoln', years: [1861,1862,1863,1864,1865]}]
+
+president.forEach((president) => 
+{
+    console.log(president.name)
+    console.log(president.years)
+    
+})
+
